18328ad0bf289bf8f799ebac710fbef9099abb50
chethan.narayana <chethan.narayana10@gmail.com>
2021-07-26 17:15:26 +0530

    pushing the code to include main for testing

--- src/main/java/com/shoppingcart/action/Purchase.java
+++ src/main/java/com/shoppingcart/action/Purchase.java
@@ -1,7 +1,18 @@
 package com.shoppingcart.action;
 
+import com.shoppingcart.purchase.Order;
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+import java.math.BigInteger;
+
 public class Purchase {
     public static void main(String[] args) {
         System.out.println("hello");
+        Order order = new Order();
+        order.addProduct(BigInteger.valueOf(122222222));
+        order.addProduct(BigInteger.valueOf(122222222));
+        order.addProduct(BigInteger.valueOf(122222232));
+        order.checkOut();
+
     }
 }
